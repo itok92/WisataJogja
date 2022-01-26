a99078a668f1d8022a94bb198c2a634306606b8f
Itok <apuruhito.kodrat@gmail.com>
2022-01-26 15:15:55 +0700

    initial commit

---
+++ app/src/main/java/itok/wisatajogja/MainActivity.kt
@@ -0,0 +1,11 @@
+package itok.wisatajogja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
